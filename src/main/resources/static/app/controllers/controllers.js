8d20d7ecf9b3ce75e22dcc980a6d452a040043e7
miten mehta <imiten@yahoo.com>
2016-05-15 18:10:33 +0530

    unit tested

--- src/main/resources/static/app/controllers/controllers.js
+++ src/main/resources/static/app/controllers/controllers.js
@@ -15,8 +15,6 @@ app.controller('backupsController', function($scope, $routeParams, configuration
        function(data, status) {
          console.log($routeParams.backupID + ":" + data.name + ":" +  data.sourceIP);
          $scope.editBackup = data;
-         //$scope.editBackup.name = data.name;
-         //$scope.editBackup.srcip = data.srcip;
        }
       );
 
@@ -24,23 +22,14 @@ app.controller('backupsController', function($scope, $routeParams, configuration
   }//init
 
   $scope.insertBackup = function () {
-        //var name = $scope.newBackup.name;
-        //var srcip = $scope.newBackup.srcip;
-      console.log('insertBackup' + JSON.stringify($scope.newBackup));
-
-        configurationFactory.insertConfiguration($scope.newBackup).success( function(data, status) {
+       configurationFactory.insertConfiguration($scope.newBackup).success( function(data, status) {
           $scope.backups.push(data);
-          console.log('insertBackup Resp:' + JSON.stringify(data));
         });
-        //$scope.newBackup.name = '';
-        //$scope.newBackup.srcip = '';
         $scope.newBackup = {};
+     
     };
 
   $scope.updateBackup = function () {
-        //var id = $scope.currentBackup;
-        //var name = $scope.editBackup.name;
-        //var srcip = $scope.editBackup.srcip;
 	  configurationFactory.updateConfiguration($scope.editBackup).success(
           function(data, status) {
            for (var j=0; j< $scope.backups.length; j++) {
@@ -51,8 +40,6 @@ app.controller('backupsController', function($scope, $routeParams, configuration
              console.log("update:" + JSON.stringify(data));
            }//for
           });
-        //$scope.newBackup.name = '';
-        //$scope.newBackup.srcip = '';
     };
 
   $scope.deleteBackup = function(id, index) {
